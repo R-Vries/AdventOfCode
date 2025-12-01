7c287233a8773d133758efa74388dea2f69422b4
Rick <r.devries-13@student.utwente.nl>
2025-12-01 09:18:51 +0100

    2025 day 1 - cleaning up

--- Kotlin/src/main/kotlin/2025/Day01.kt
+++ Kotlin/src/main/kotlin/2025/Day01.kt
@@ -1,27 +1,20 @@
 package aoc.`2025`
 
 import aoc.Solver
-import kotlin.math.abs
 
 object Day01: Solver<List<Move>>(2025, 1, 3, 16) {
-    override fun parse(input: List<String>): List<Move> {
-        val result = mutableListOf<Move>()
-        for (line in input) {
-            result.add(Move(line.take(1), line.substring(1).toInt()))
-        }
-        return result
-    }
+    override fun parse(input: List<String>): List<Move> =
+        input.map { line -> Move(line.take(1), line.substring(1).toInt()) }
 
     override fun part1(data: List<Move>): Int {
         var visitedZero = 0
         var current = 50
-        for (move in data) {
-            if (current == 0) {
-                visitedZero++
-            }
-            when (move.direction) {
-                "L" -> current = Math.floorMod(current - move.amount, 100)
-                "R" -> current = Math.floorMod(current + move.amount, 100)
+        data.forEach { move ->
+            if (current == 0) visitedZero++
+            current = when (move.direction) {
+                "L" -> Math.floorMod(current - move.amount, 100)
+                "R" -> Math.floorMod(current + move.amount, 100)
+                else -> current
             }
         }
         return visitedZero
@@ -32,7 +25,7 @@ object Day01: Solver<List<Move>>(2025, 1, 3, 16) {
         var current = 50
         for (move in data) {
             val change = when (move.direction) {
-                "L" -> -1 * move.amount
+                "L" -> -move.amount
                 "R" ->  move.amount
                 else -> 0
             }
@@ -47,11 +40,9 @@ object Day01: Solver<List<Move>>(2025, 1, 3, 16) {
         var visitedZero = 0
         val step = if (move.direction == "L") -1 else 1
 
-        for (i in 1..move.amount) {
+        repeat(move.amount) {
             cur = Math.floorMod(cur + step, 100)
-            if (cur == 0) {
-                visitedZero++
-            }
+            if (cur == 0) visitedZero++
         }
         return visitedZero
     }
