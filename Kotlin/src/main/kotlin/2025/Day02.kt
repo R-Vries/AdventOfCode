73d89d18e6c5c576e177db267dfd47e641773458
Rick <r.devries-13@student.utwente.nl>
2025-12-02 15:25:55 +0100

    2025 day 2 (improvements)

--- Kotlin/src/main/kotlin/2025/Day02.kt
+++ Kotlin/src/main/kotlin/2025/Day02.kt
@@ -16,47 +16,32 @@ object Day02: Solver<List<Day02.Range>>() {
                 }
             }
 
-
-    override fun part1(data: List<Range>): Long {
-        var result = 0L
-        for (r in data) {
-            for (id in r.start..r.end) {
-                val idString = id.toString()
-                if (idString.length % 2 != 0) continue
-
-                val middle = idString.length / 2
-                if (idString.take(middle) == idString.substring(middle)) {
-                    result += id
-                }
-            }
+    override fun part1(data: List<Range>): Long =
+        data.sumOf { r ->
+            (r.start..r.end).filter { id ->
+                val s = id.toString()
+                s.length % 2 == 0 && s.take(s.length / 2) == s.drop(s.length / 2)
+            }.sum()
         }
-        return result
-    }
 
-    override fun part2(data: List<Range>): Long {
-        var result = 0L
-        for (r in data) {
-            for (id in r.start..r.end) {
-                val idString = id.toString()
-
-                for (i in 1..idString.length / 2) {
-                    val sublists = idString.chunked(i)
-                    if (sublists.all { it == sublists.first() }) {
-                        result += id
-                        break
+    override fun part2(data: List<Range>): Long =
+        data.sumOf { r ->
+            (r.start..r.end).filter {id ->
+                val s = id.toString()
+                (1..s.length / 2).any { size ->
+                    s.chunked(size).let { chunks ->
+                        chunks.all { it == chunks.first() }
                     }
                 }
-            }
+            }.sum()
         }
-        return result
-    }
 
     data class Range(val start: Long, val end: Long)
 }
 
 fun main() {
     val io = IOManager(2025, 2)
-    val tester = Tester(Day02, io, 1227775554, 4174379265)
+    val tester = Tester(Day02, io, 1227775554L, 4174379265)
     val runner = Runner(Day02, io)
     tester.runTests()
     runner.run()
