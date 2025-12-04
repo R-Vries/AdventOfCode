c09cd8c34439a328b37f70e47d7efa265e6b7ad1
Rick <r.devries-13@student.utwente.nl>
2025-12-04 09:24:01 +0100

    Grid Class documentation

--- Kotlin/src/main/kotlin/Grid.kt
+++ Kotlin/src/main/kotlin/Grid.kt
@@ -1,5 +1,35 @@
 package aoc
 
+/**
+ * A 2D grid of mutable lists.
+ *
+ * The grid is constructed from the AoC input.
+ * The grid can contain elements of any [T].
+ * A constructor for a Grid<Char> already exists
+ *
+ * The grid provides convenient access basic iteration functions, working with [Coordinate]s and utilities such as
+ * counting neighbors.
+ *
+ * ### Basic usage:
+ * ```
+ * val grid = Grid(input)      // Grid<Char>
+ *
+ * val value = grid[Coordinate(2, 3)]
+ * ```
+ *
+ * ### Iteration:
+ * ```
+ * for (value in grid) { ... }                  // iterate values
+ * for (coord in grid.coordinates()) { ... } // iterate coordinates
+ * ```
+ *
+ * @param T the element type stored in the grid
+ *
+ * @property data the underlying 2D structure containing grid values
+ * @property rows the number of rows in the grid
+ * @property cols the number of columns in the grid
+ */
+
 class Grid<T>(
     private val data: MutableList<MutableList<T>>
 ): Iterable<T> {
@@ -32,14 +62,13 @@ class Grid<T>(
                     yield(Coordinate(i, j))
         }
 
-
     fun inBounds(i: Int, j: Int): Boolean =
         i in 0 until rows && j in 0 until cols
 
     fun inBounds(c: Coordinate): Boolean =
         inBounds(c.i, c.j)
 
-    /** Returns all 8 neighbours  */
+    /** Returns all 8 surrounding neighbours of this [Coordinate]  */
     fun getNeighbours(c: Coordinate): List<Coordinate> =
         listOf(
             Coordinate(c.i - 1, c.j - 1),
@@ -54,28 +83,16 @@ class Grid<T>(
 
     override fun toString(): String =
         data.joinToString("\n") { row -> row.joinToString("") }
-
-
 }
 
+/** Class representing a coordinate in a grid */
 data class Coordinate(val i: Int, val j: Int)
 
 /**
  * Constructs a Char Grid using the regular input.
  *
  * @param input the lines of the input file
- * @return a new Grid<Char>
+ * @return a new [Grid] of [Char]s
  */
-fun Grid(input: List<String>): Grid<Char> {
-    val data = input.map { line -> line.toMutableList() }.toMutableList()
-    return Grid(data)
-}
-
-fun <T> Grid(input: List<String>, transform: (Char) -> T): Grid<T> {
-    val data = input.map { line ->
-        line.map(transform).toMutableList()
-    }.toMutableList()
-
-    return Grid(data)
-}
-
+fun Grid(input: List<String>): Grid<Char> =
+    Grid(input.map { line -> line.toMutableList() }.toMutableList())
