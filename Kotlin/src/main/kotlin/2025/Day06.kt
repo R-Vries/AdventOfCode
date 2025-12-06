af85c52d18741ecfaf90aac150be5776d508fcd5
Rick <r.devries-13@student.utwente.nl>
2025-12-06 10:49:20 +0100

    2025 day 6 (improvements)

--- Kotlin/src/main/kotlin/2025/Day06.kt
+++ Kotlin/src/main/kotlin/2025/Day06.kt
@@ -9,64 +9,54 @@ object Day06: Solver<List<Day06.Calculation>>() {
     override fun parse(input: List<String>): List<Calculation> {
         // finding columns with only spaces
         val spaceIndices = findSpaces(input)
-        val result = mutableListOf<Calculation>()
-        for (col in input.first().trim().split("\\s+".toRegex())) {
-            result.add(Calculation(mutableListOf(), ""))
-        }
-        for (line in input) {
-            if (line.startsWith('*') || line.startsWith('+')) {
-                val instructions = line.trim().split("\\s+".toRegex())
-                for (i in 0..instructions.lastIndex) {
-                    result[i].operator = instructions[i]
-                }
-            } else {
-                val numbers = splitAtIndices(line, spaceIndices)
-                for (i in 0..numbers.lastIndex) {
-                    result[i].numbers.add(numbers[i])
-                }
-            }
+        // add the correct amount of column lists
+        val cols = input.first().trim().split("\\s+".toRegex()).size
+        val result = MutableList(cols) { Calculation(mutableListOf(), null, 0)}
 
+        // fill column lists with numbers
+        input.dropLast(1).forEach { line ->
+            splitAtIndices(line, spaceIndices).forEachIndexed { i, number ->
+                result[i].numbers.add(number)
+            }
         }
+        // add instructions
+        val instructions = input.last().trim().split("\\s+".toRegex())
+        instructions.forEachIndexed { i, operator ->
+            if (operator == "+") {
+                result[i].operator = { a: Long, b: Long -> a + b }
+                result[i].default = 0
+            } else {
+                result[i].operator = { a: Long, b: Long -> a * b }
+                result[i].default = 1
+            }
+        }
+
         return result
     }
 
     override fun part1(data: List<Calculation>): Number =
         data.sumOf { calc ->
-            if (calc.operator == "*") calc.numbers.map { it.trim().toLong() }
-                .reduce(Long::times)
-            else calc.numbers.sumOf { it.trim().toLong() }
+            calc.numbers.map { it.trim().toLong() }.reduce { a, b -> calc.operator!!(a, b) }
         }
 
     override fun part2(data: List<Calculation>): Number =
         data.sumOf { calc ->
-            if (calc.operator == "*") {
-                var result = 1L
-                // reading numbers vertically
-                for (i in 0..calc.numbers[0].lastIndex) {
-                    var verticalNumber = ""
-                    for (n in calc.numbers) {
-                        if (n[i] == ' ') continue
-                        else verticalNumber += n[i]
-                    }
-                    result *= verticalNumber.toLong()
+            (0..calc.numbers[0].lastIndex).map { i ->
+                var verticalNumber = ""
+                for (n in calc.numbers) {
+                    if (n[i] == ' ') continue
+                    else verticalNumber += n[i]
                 }
-                result
-            } else {
-                var result = 0L
-                // reading numbers vertically
-                for (i in 0..calc.numbers[0].lastIndex) {
-                    var verticalNumber = ""
-                    for (n in calc.numbers) {
-                        if (n[i] == ' ') continue
-                        else verticalNumber += n[i]
-                    }
-                    result += verticalNumber.toLong()
-                }
-                result
+                verticalNumber.toLong()
             }
+                .fold(calc.default, calc.operator!!)
         }
 
-    data class Calculation(val numbers: MutableList<String>, var operator: String)
+    data class Calculation(
+        val numbers: MutableList<String>,
+        var operator: ((Long, Long) -> Long)?,
+        var default: Long
+    )
 
     fun splitAtIndices(s: String, indices: List<Int>): List<String> {
         val result = mutableListOf<String>()
@@ -81,23 +71,10 @@ object Day06: Solver<List<Day06.Calculation>>() {
         return result
     }
 
-    private fun findSpaces(input: List<String>): MutableList<Int> {
-        val spaceIndices = mutableListOf<Int>()
-        for (i in 0..input[0].trim().lastIndex) {
-            if (input[0][i] == ' ') {
-                spaceIndices.add(i)
-            }
-        }
-        val result = spaceIndices.toMutableList()
-        for (line in input.drop(1)) {
-            for (i in spaceIndices) {
-                if (line[i] != ' ') {
-                    result.remove(i)
-                }
-            }
-        }
-        return result
-    }
+    private fun findSpaces(input: List<String>): List<Int> =
+        input[0].trim().withIndex()
+            .filter { (i, _) -> input.all { line -> line[i] == ' '} }
+            .map{ it.index }
 }
 
 fun main() {
